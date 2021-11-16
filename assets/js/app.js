ab6022a7e6ec34b471e62d8e60d99a6b4cf1b1af
timduran <durantimothy21@gmail.com>
2021-11-15 18:40:54 -0800

    added questions

---
+++ assets/js/app.js
@@ -0,0 +1,41 @@
+const questions = [
+
+  { //1 
+    question: "What is Javascript file extension?", choices: [
+      '.Java',
+      '.exe',
+      '.zip',
+      '.js',
+    ],
+    answer: 4
+  },
+  { // 2
+    question: "What are variables used for in JavaScript Programs?",
+    choices: [
+      "Storing numbers, dates, or other values",
+      "Varying randomly",
+      "Causing high-school algebra flashbacks",
+      "None of the above",
+    ],
+    answer: 1
+  },
+  {// 3
+    question: "What is the difference between const and let?",
+    choices: [
+      "Const can be reassigned and so can let",
+      "Const cannot be reassigned but let can",
+      "Neither can be reassigned",
+      "Both can be reassigned",
+    ],
+    answer: 2
+  },
+  {// 4 
+    question: "Which of the following attribute can hold the JavaScript version?",
+    choices: [
+      "LANGUAGE",
+      "SCRIPT",
+      "VERSION",
+      "None of the above",
+    ],
+    answer: 1
+  },
