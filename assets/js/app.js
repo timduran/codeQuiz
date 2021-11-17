3f59296013188e63510991c2398907ce0fe956e9
timduran <durantimothy21@gmail.com>
2021-11-16 16:04:21 -0800

    new updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,13 @@
+var startBtn = document.getElementById('start')
+var questionContainer = document.getElementById('questions')
+var scoreEl = document.getElementById('score')
+var questionTitle = document.getElementById('questionTitle');
+var choicesEl = document.getElementById('choices');
+var timeEl = document.getElementById('time');
+
+var currentQuestionInd = 0;
+var timerId;
+
 const questions = [
 
   { //1 
@@ -7,7 +17,7 @@ const questions = [
       '.zip',
       '.js',
     ],
-    answer: 4
+    answer: '.js'
   },
   { // 2
     question: "What are variables used for in JavaScript Programs?",
@@ -17,7 +27,7 @@ const questions = [
       "Causing high-school algebra flashbacks",
       "None of the above",
     ],
-    answer: 1
+    answer: "Varying randomly"
   },
   {// 3
     question: "What is the difference between const and let?",
@@ -27,7 +37,7 @@ const questions = [
       "Neither can be reassigned",
       "Both can be reassigned",
     ],
-    answer: 2
+    answer: "Const cannot be reassigned but let can"
   },
   {// 4 
     question: "Which of the following attribute can hold the JavaScript version?",
@@ -37,5 +47,98 @@ const questions = [
       "VERSION",
       "None of the above",
     ],
-    answer: 1
+    answer: "LANGUAGE"
   },
+]
+var time = questions.length * 15;
+var currentQuestion = questions[currentQuestionInd];
+
+
+// const questions = ''
+
+
+// Start Game 
+const newGame = () => {
+  startBtn.style.display = "none"
+  // document.getElementById('button1').style.display = "block"
+  // document.getElementById('button2').style.display = "block"
+  // document.getElementById('button3').style.display = "block"
+  // document.getElementById('button4').style.display = "block"
+  timerId = setInterval(clockTick, 1000)
+  timeEl.textContent = time;
+  getQuestion()
+}
+
+const getQuestion = () => {
+  // variable for current question
+  var currentQuestion = questions[currentQuestionInd];
+  questionTitle.textContent = currentQuestion.question;
+
+  choicesEl.innerHTML = "";
+
+  currentQuestion.choices.forEach(function(choice, i) {
+    var choiceBtn = document.createElement("button");
+    choiceBtn.setAttribute('class', 'choice')
+    choiceBtn.setAttribute('value', choice);
+
+    choiceBtn.textContent = i + 1 + '. ' + choice;
+
+    choiceBtn.addEventListener('click', questionClick)
+
+    choicesEl.appendChild(choiceBtn)
+  })
+}
+
+// Clock tick function
+const clockTick= () => {
+  time--;
+  timeEl.textContent = time;
+  if(time <= 0 ) {
+    quizEnd();
+  }
+}
+
+
+// Question Click Function
+const questionClick = (event) => {
+  event.preventDefault();
+  // console.log(event.target.value)
+  if(event.target.value !== questions[currentQuestionInd].answer) {
+    time -= 15;
+    if(time<0) {
+      time = 0;
+    }
+    timeEl.textContent = time;
+  } 
+
+  currentQuestionInd++;
+  if(currentQuestionInd === questions.length) {
+    quizEnd();
+  } else {
+    getQuestion();
+  }
+}
+
+// Make a highscore page where hoghscores are displayd
+// Retrieve highscores from localstorage using localStorage.getItem and print them to page
+// Make a function to save a highscore when submit initials button is clicked and do localStorage.setItem
+// var newScore = {
+//   score: time,
+//   initials: initials
+// }
+// var highscores = JSON.parse(window.localStorage.getItem('highscores')) || [];
+// window.localStorage.setItem('highscores', JSON.stringify(highscores))
+
+// quizEnd function
+const quizEnd = () => {
+  clearInterval(timerId);
+  var endScreen = document.getElementById('endScreen');
+  endScreen.removeAttribute('class');
+
+  var finalScore = document.getElementById('finalScore');
+  finalScore.textContent = time;
+
+  questionContainer.setAttribute('class', 'hide')
+}
+
+startBtn.addEventListener('click', newGame);
